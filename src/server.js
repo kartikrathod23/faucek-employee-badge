3d6eaeae0ebe4604bfbf640e4a5ee526d63be454
kartik rathod <rathodkartik293@gmail.com>
2025-06-16 16:06:46 +0530

    remove city validation logic

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
-// export const url = "https://badge-generator-backend.vercel.app"
-export const url = "http://localhost:5000"
+export const url = "https://badge-generator-backend.vercel.app"
+// export const url = "http://localhost:5000"
